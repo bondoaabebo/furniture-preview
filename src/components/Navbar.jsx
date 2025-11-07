868910d4eb16440d7a0f7ecd37a742e867a6a83e
bondoaabebo <galal.galgola@gmail.com>
2025-11-07 23:28:46 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import "../pages/Navbar.css";
+import "./Navbar.css";
 
 function Navbar() {
   const [open, setOpen] = useState(false);
